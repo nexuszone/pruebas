32ff03d1df2e00a9883a9e5ee4701059604529ea
nexuszone <nexuszonemsl@gmail.com>
2026-09-20 19:15:53 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // NeXus ZonE Radio - Service Worker para habilitar instalación PWA
-const CACHE_NAME = 'nexuszone-cache-v1.1';
+const CACHE_NAME = 'nexuszone-cache-v1.2';
 const CORE_ASSETS = [
   './',
   './index.html',
