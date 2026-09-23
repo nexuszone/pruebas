0447699855fe036a07a58514851821e626e64c02
nexuszone <nexuszonemsl@gmail.com>
2026-09-23 22:38:46 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// NeXus ZonE Radio - Service Worker v1.1.83
-const CACHE_NAME = 'nexuszone-cache-v1.1.83';
+// NeXus ZonE Radio - Service Worker v1.1.84
+const CACHE_NAME = 'nexuszone-cache-v1.1.84';
 const CORE_ASSETS = [
   './',
   './index.html',
